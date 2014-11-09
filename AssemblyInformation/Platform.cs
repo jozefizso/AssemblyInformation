b263f241bd1c3cd871b132f2092f92bf6b477e04
Jozef Izso <jozef.izso@gmail.com>
2014-11-09 02:58:14 +0100

    Refactored and cleaned up code.

--- AssemblyInformation/Platform.cs
+++ AssemblyInformation/Platform.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
-namespace AssemblyInformation 
+namespace AssemblyInformation
 {
-    // ReSharper disable InconsistentNaming
-    public enum MachineType : ushort 
+    //// ReSharper disable InconsistentNaming
+    [SuppressMessage("StyleCopPlus.StyleCopPlusRules", "SP0100:AdvancedNamingRules", Justification = "Reviewed. Suppression is OK here.")]
+    public enum MachineType : ushort
     {
         IMAGE_FILE_MACHINE_UNKNOWN = 0x0,
         IMAGE_FILE_MACHINE_AM33 = 0x1d3,
@@ -27,25 +29,33 @@ namespace AssemblyInformation
         IMAGE_FILE_MACHINE_THUMB = 0x1c2,
         IMAGE_FILE_MACHINE_WCEMIPSV2 = 0x169,
     }
-    // ReSharper restore InconsistentNaming
+    //// ReSharper restore InconsistentNaming
 
-    class Platform 
+    internal static class Platform
     {
-        public static MachineType GetDllMachineType(string dllPath) 
+        /// <summary>
+        /// PE header starts with "PE\0\0" = 0x50 0x45 0x00 0x00 (little endian).
+        /// </summary>
+        private const int PE_HEADER_LITTLE_ENDIAN = 0x00004550;
+
+        public static MachineType GetDllMachineType(string dllPath)
         {
-            //see http://www.microsoft.com/whdc/system/platform/firmware/PECOFF.mspx
-            //offset to PE header is always at 0x3C
-            //PE header starts with "PE\0\0" =  0x50 0x45 0x00 0x00
-            //followed by 2-byte machine type field (see document above for enum)
-            using (var fs = new FileStream(dllPath, FileMode.Open, FileAccess.Read)) 
+            // see http://www.microsoft.com/whdc/system/platform/firmware/PECOFF.mspx
+            // offset to PE header is always at 0x3C
+            // PE header starts with "PE\0\0" =  0x50 0x45 0x00 0x00
+            // followed by 2-byte machine type field (see document above for enum)
+            using (var fs = new FileStream(dllPath, FileMode.Open, FileAccess.Read))
             {
                 var br = new BinaryReader(fs);
                 fs.Seek(0x3c, SeekOrigin.Begin);
                 Int32 peOffset = br.ReadInt32();
                 fs.Seek(peOffset, SeekOrigin.Begin);
                 UInt32 peHead = br.ReadUInt32();
-                if (peHead != 0x00004550) // "PE\0\0", little-endian
+                if (peHead != PE_HEADER_LITTLE_ENDIAN)
+                {
                     throw new BadImageFormatException("Unable to determine the assembly's type. Can't find PE header");
+                }
+
                 var machineType = (MachineType)br.ReadUInt16();
                 br.Close();
                 fs.Close();
@@ -54,13 +64,15 @@ namespace AssemblyInformation
         }
 
         /// <summary>
-        /// returns true if the dll is 64-bit, false if 32-bit, and null if unknown
+        /// Checks if assembly is 64-bit.
         /// </summary>
-        /// <param name="dllPath"></param>
-        /// <returns></returns>
-        public static bool? Is64BitAssembly(string dllPath) 
+        /// <param name="dllPath">Path to the assembly.</param>
+        /// <returns>
+        /// Returns <c>true</c> if the dll is 64-bit, <c>false</c> if 32-bit, and <c>null</c> if unknown.
+        /// </returns>
+        public static bool? Is64BitAssembly(string dllPath)
         {
-            switch (GetDllMachineType(dllPath)) 
+            switch (GetDllMachineType(dllPath))
             {
                 case MachineType.IMAGE_FILE_MACHINE_AMD64:
                 case MachineType.IMAGE_FILE_MACHINE_IA64:
@@ -71,19 +83,12 @@ namespace AssemblyInformation
                     return null;
             }
         }
-        public static bool IsRunningAs64Bit 
+
+        public static bool IsRunningAs64Bit
         {
             get
             {
                 return Environment.Is64BitProcess;
-                //var currentAssembly = Assembly.GetCallingAssembly();
-                //PortableExecutableKinds kinds;
-                //ImageFileMachine imgFileMachine;
-                //currentAssembly.ManifestModule.GetPEKind(out kinds, out imgFileMachine);
-
-                //if (kinds == PortableExecutableKinds.NotAPortableExecutableImage) return false;
-                //var is64Bit = ((kinds & PortableExecutableKinds.PE32Plus) == PortableExecutableKinds.PE32Plus);
-                //return is64Bit;
             }
         }
     }
--- AssemblyInformation/Program.cs
+++ AssemblyInformation/Program.cs
@@ -6,35 +6,36 @@ using System.Windows.Forms;
 
 namespace AssemblyInformation
 {
-    class Program
+    using System.Threading;
+
+    public class Program
     {
         public const string ApplicationPathx86 = "AssemblyInformation32.exe";
         public const string ApplicationPathx64 = "AssemblyInformation64.exe";
 
         [STAThread]
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Application.ThreadException += ApplicationThreadException;
             AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainTypeResolve;
-            
+
             if (args.Length == 1)
             {
                 string filePath = args.GetValue(0).ToString();
-                string assemblyullName = Path.GetFullPath(filePath);
+                string assemblyFullName = Path.GetFullPath(filePath);
                 bool? is64Bit = Platform.Is64BitAssembly(filePath);
-                bool filePathIs64Bit = (is64Bit.HasValue && is64Bit.Value);
+                bool filePathIs64Bit = is64Bit.HasValue && is64Bit.Value;
                 bool currentProcessIs64Bit = Platform.IsRunningAs64Bit;
 
-                bool spanProcess = (currentProcessIs64Bit != filePathIs64Bit);
+                bool spanProcess = currentProcessIs64Bit != filePathIs64Bit;
 
                 try
                 {
                     if (!spanProcess)
                     {
-                        
-                        //required to change directory for loading referenced assemblies
+                        // required to change directory for loading referenced assemblies
                         Environment.CurrentDirectory = Path.GetDirectoryName(filePath);
-                        Assembly assembly = Assembly.LoadFile(assemblyullName);
+                        Assembly assembly = Assembly.LoadFile(assemblyFullName);
 
                         Application.EnableVisualStyles();
                         Application.SetCompatibleTextRenderingDefault(false);
@@ -42,40 +43,37 @@ namespace AssemblyInformation
                     }
                     else
                     {
-                        if(filePathIs64Bit && !Environment.Is64BitOperatingSystem)
+                        if (filePathIs64Bit && !Environment.Is64BitOperatingSystem)
                         {
-                            MessageBox.Show(Resource.BitnessMismatch, Resource.AppName,
-                                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            MessageBox.Show(Resource.BitnessMismatch, Resource.AppName, MessageBoxButtons.OK, MessageBoxIcon.Error);
                             return;
                         }
+
                         string launchPath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
-                        Debug.Assert(launchPath != null);
+                        Debug.Assert(launchPath != null, "launchPath != null - Failed to get directory name of current process.");
 
                         string otherBitnessPath = Path.Combine(launchPath, currentProcessIs64Bit ? ApplicationPathx86 : ApplicationPathx64);
 
-                        if(!File.Exists(otherBitnessPath))
+                        if (!File.Exists(otherBitnessPath))
                         {
-                            MessageBox.Show(string.Format(Resource.FailedToLocateFile, otherBitnessPath), Resource.AppName,
-                                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            var message = string.Format(Resource.FailedToLocateFile, otherBitnessPath);
+                            MessageBox.Show(message, Resource.AppName, MessageBoxButtons.OK, MessageBoxIcon.Error);
                             return;
                         }
-                        
 
                         var psi = new ProcessStartInfo();
                         psi.FileName = otherBitnessPath;
-                        if (!assemblyullName.Contains(" "))
-                            psi.Arguments = args[0];
-                        else
-                            psi.Arguments = string.Format("\"{0}\"", assemblyullName);
+                        psi.Arguments = !assemblyFullName.Contains(" ") ? args[0] : string.Format("\"{0}\"", assemblyFullName);
+
                         psi.UseShellExecute = true;
                         var process = Process.Start(psi);
-                        //process.WaitForExit();
+                        ////process.WaitForExit();
                     }
                 }
                 catch (Exception ex)
                 {
-                    //if try to load win32 binary, then it will throw BadImageFormat exception...
-                    //which doesn't contain any HResult. So, just search for it.
+                    // if try to load win32 binary, then it will throw BadImageFormat exception...
+                    // which doesn't contain any HResult. So, just search for it.
                     if (ex.Message.Contains(Resource.NotDotNetAssemblyErrorMessage) || ex.Message.Contains("0x80131018"))
                     {
                         MessageBox.Show(string.Format(Resource.NotDotNetAssembly, filePath), Resource.AppName, MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -92,7 +90,7 @@ namespace AssemblyInformation
             }
         }
 
-        static Assembly CurrentDomainTypeResolve(object sender, ResolveEventArgs args)
+        private static Assembly CurrentDomainTypeResolve(object sender, ResolveEventArgs args)
         {
             if (null != args && !String.IsNullOrEmpty(args.Name))
             {
@@ -106,16 +104,23 @@ namespace AssemblyInformation
                         {
                             return Assembly.LoadFile((new FileInfo(name)).FullName);
                         }
-                        catch (ArgumentException) { }
-                        catch (IOException) { }
-                        catch (BadImageFormatException) { }
+                        catch (ArgumentException)
+                        {
+                        }
+                        catch (IOException)
+                        {
+                        }
+                        catch (BadImageFormatException)
+                        {
+                        }
                     }
                 }
             }
+
             return null;
         }
 
-        static void ApplicationThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        private static void ApplicationThreadException(object sender, ThreadExceptionEventArgs e)
         {
             MessageBox.Show(string.Format(Resource.LoadError, e.Exception.Message), Resource.AppName, MessageBoxButtons.OK);
             Application.Exit();
