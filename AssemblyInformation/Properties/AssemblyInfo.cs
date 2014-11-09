a4c3b9fea97324c896a8d8e6cdefc5c6ac155f46
Jozef Izso <jozef.izso@gmail.com>
2014-11-09 02:14:38 +0100

    Cleaned up AssemblyInfo.cs file.

--- AssemblyInformation/Properties/AssemblyInfo.cs
+++ AssemblyInformation/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Information about an assembly.
 [assembly: AssemblyTitle(".NET Assembly Information")]
 [assembly: AssemblyDescription("Displays information on a .NET Assembly (DLL).  Developed by Rotem Bloom.  Version 1.1 supports .NET 4.0.\r\n\r\nContributed by Ashutosh Bhawasinka (www.ashusoft.com) - Added Assembly Kind, target platform, dependency tree, recursive dependency list and launching Assembly Information for dependency.")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// Configure COM visibility and typelib identifier.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cfe3b171-9c81-4439-bd6b-58871955599d")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// Version information for assembly.
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyFileVersion("2.1.0.0")]
