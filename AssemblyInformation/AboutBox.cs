342cfcdc97dd1a2af5f0db2feddbcdb242fdaea8
Jozef Izso <jozef.izso@gmail.com>
2014-11-09 03:12:24 +0100

    Refactored AboutBox class.

--- AssemblyInformation/AboutBox.cs
+++ AssemblyInformation/AboutBox.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
 
 namespace AssemblyInformation
 {
-    partial class AboutBox : Form
+    internal partial class AboutBox : Form
     {
         public AboutBox()
         {
-            InitializeComponent();
-            this.Text = String.Format("About {0}", AssemblyTitle);
-            this.labelProductName.Text = AssemblyProduct;
-            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
-            this.labelCopyright.Text = AssemblyCopyright;
-            this.labelCompanyName.Text = AssemblyCompany;
-            this.textBoxDescription.Text = AssemblyDescription;
+            this.InitializeComponent();
+            this.Text = String.Format("About {0}", this.AssemblyTitle);
+            this.labelProductName.Text = this.AssemblyProduct;
+            this.labelVersion.Text = String.Format("Version {0}", this.AssemblyVersion);
+            this.labelCopyright.Text = this.AssemblyCopyright;
+            this.labelCompanyName.Text = this.AssemblyCompany;
+            this.textBoxDescription.Text = this.AssemblyDescription;
         }
 
         #region Assembly Attribute Accessors
@@ -27,13 +28,14 @@ namespace AssemblyInformation
                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
                 if (attributes.Length > 0)
                 {
-                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    var titleAttribute = (AssemblyTitleAttribute)attributes[0];
                     if (titleAttribute.Title != "")
                     {
                         return titleAttribute.Title;
                     }
                 }
-                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+
+                return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
             }
         }
 
@@ -49,12 +51,7 @@ namespace AssemblyInformation
         {
             get
             {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+                return this.GetAssemblyInformation<AssemblyDescriptionAttribute>(attr => attr.Description);
             }
         }
 
@@ -62,12 +59,7 @@ namespace AssemblyInformation
         {
             get
             {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyProductAttribute)attributes[0]).Product;
+                return this.GetAssemblyInformation<AssemblyProductAttribute>(attr => attr.Product);
             }
         }
 
@@ -75,12 +67,7 @@ namespace AssemblyInformation
         {
             get
             {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+                return this.GetAssemblyInformation<AssemblyCopyrightAttribute>(attr => attr.Copyright);
             }
         }
 
@@ -88,13 +75,19 @@ namespace AssemblyInformation
         {
             get
             {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+                return this.GetAssemblyInformation<AssemblyCompanyAttribute>(attr => attr.Company);
             }
+        }
+
+        private string GetAssemblyInformation<T>(Func<T, string> valueSelectorFunc) where T : Attribute
+        {
+            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(T), false);
+            if (attributes.Length == 0)
+            {
+                return "";
+            }
+
+            return valueSelectorFunc((T)attributes[0]);
         }
         #endregion
     }
