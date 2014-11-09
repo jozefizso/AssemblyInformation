f419611b4c0941dc70e08f4277abef774eaf8dbe
Jozef Izso <jozef.izso@gmail.com>
2014-11-09 01:31:19 +0100

    Build script will compile AssemblyInformation in x86 and x64 configurations.

--- BuildScript/BuildAssemblyInformation.cmd
+++ BuildScript/BuildAssemblyInformation.cmd
@@ -1,37 +1,52 @@
 @echo off
 setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat">nul 2>nul
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat">nul 2>nul
+
+@if "%VS120COMNTOOLS%"=="" goto error_no_VS120COMNTOOLSDIR
+
+call "%VS120COMNTOOLS%vsvars32.bat"
 
 cd /d %~dp0
 cd..
 set TEMP_BUILD_LOG=BuildLog.txt
 Echo Building Assembly Information
-msbuild /p:configuration=Release AssemblyInformation.sln >%TEMP_BUILD_LOG%
+msbuild.exe /p:Configuration=Release;Platform=x86 AssemblyInformation.sln >%TEMP_BUILD_LOG%
 if errorlevel 1 (
-	echo Failed to build AssemblyInformation in Release Mode.
-	type %TEMP_BUILD_LOG%
-	if not defined NOPAUSE pause
-	exit /b 1
+    echo "Failed to build AssemblyInformation in Release (x86) mode."
+    type %TEMP_BUILD_LOG%
+    if not defined NOPAUSE pause
+    exit /b 1
+)
+
+msbuild.exe /p:Configuration=Release;Platform=x64 AssemblyInformation.sln >%TEMP_BUILD_LOG%
+if errorlevel 1 (
+    echo "Failed to build AssemblyInformation in Release (x64) mode."
+    type %TEMP_BUILD_LOG%
+    if not defined NOPAUSE pause
+    exit /b 2
 )
 
 Echo Cleaning Up Temporary files
 
-del .\bin\Release\AssemblyInformationx64.exp>nul 2>nul
-del .\bin\Release\AssemblyInformationx64.ilk>nul 2>nul
-del .\bin\Release\AssemblyInformationx64.lib>nul 2>nul
-del .\bin\Release\AssemblyInformationx64.pdb>nul 2>nul
-del .\bin\Release\AssemblyInformationx64.exe.config>nul 2>nul
-del .\bin\Release\AssemblyInformation.exp>nul 2>nul
-del .\bin\Release\AssemblyInformation.ilk>nul 2>nul
-del .\bin\Release\AssemblyInformation.lib>nul 2>nul
-del .\bin\Release\AssemblyInformation.pdb>nul 2>nul
-del .\bin\Release\AssemblyInformation.exe.config>nul 2>nul
+del .\bin\Release\AssemblyInformation64.exp>nul 2>nul
+del .\bin\Release\AssemblyInformation64.ilk>nul 2>nul
+del .\bin\Release\AssemblyInformation64.lib>nul 2>nul
+del .\bin\Release\AssemblyInformation64.pdb>nul 2>nul
+del .\bin\Release\AssemblyInformation64.exe.config>nul 2>nul
+del .\bin\Release\AssemblyInformation32.exp>nul 2>nul
+del .\bin\Release\AssemblyInformation32.ilk>nul 2>nul
+del .\bin\Release\AssemblyInformation32.lib>nul 2>nul
+del .\bin\Release\AssemblyInformation32.pdb>nul 2>nul
+del .\bin\Release\AssemblyInformation32.exe.config>nul 2>nul
 
 del %TEMP_BUILD_LOG%>nul 2>nul
 
 rd /S /Q obj>nul 2>nul
 Echo Completed Building Assembly Information
 if not defined NOPAUSE pause
+exit /B 0
 
+:error_no_VS120COMNTOOLSDIR
+echo ERROR: Cannot determine the location of the VS Common Tools folder (%%VS120COMNTOOLS%%).
+goto end
 
+:end
